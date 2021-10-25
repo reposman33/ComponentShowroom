016810a57ecbedceb7d2003da5151a63426242c4
Marc Bakker <webdev@marcbakker.com>
2021-10-25 22:18:58 +0200

    Add Button component.

--- projects/ui-components/src/lib/components/button/button.component.ts
+++ projects/ui-components/src/lib/components/button/button.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'ui-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+	selector: 'ui-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+	@Input() active: boolean = false;
+	@Input() formAction: 'save' | 'submit' | '' = '';
 
-  constructor() { }
+	public buttonClass: string = 'button';
 
-  ngOnInit(): void {
-  }
+	constructor() {}
 
+	ngOnInit(): void {
+		this.buttonClass +=
+			this.formAction === ''
+				? this.active
+					? ' button-active'
+					: ' button-inactive'
+				: this.formAction === 'submit'
+				? ' button--submit'
+				: ' button--save';
+	}
 }
